3e664e15259b13509f8ed8812a21ce5a0bac513e
Farzad-R <farzad,roozitalab@gmail.com>
2024-08-04 14:10:37 +0000

    checked 13B input format - working on 13b training pipeline

---
+++ Notebooks/fine_tuning/fine_tune_llava_1.6_13b_on_cordv2.py
@@ -0,0 +1,359 @@
+from transformers import BitsAndBytesConfig, LlavaNextForConditionalGeneration, AutoProcessor
+from datasets import load_dataset
+import torch
+import json
+
+if torch.cuda.is_available():
+    print("PyTorch is connected to GPU.")
+    print(f"GPU Device Name: {torch.cuda.get_device_name(0)}")
+    print(f"Number of GPUs available: {torch.cuda.device_count()}")
+    print(f"Current GPU: {torch.cuda.current_device()}")
+else:
+    print("PyTorch is not connected to GPU.")
+
+MAX_LENGTH = 256
+MODEL_ID = "llava-hf/llava-v1.6-vicuna-13b-hf"
+REPO_ID = "Farzad-R/llava-v1.6-vicuna-13b-cordv2"
+NUM_WORKERS = 4
+
+processor = AutoProcessor.from_pretrained(MODEL_ID)
+processor.tokenizer.padding_side = "right" # during training, one always uses padding on the right
+
+USE_LORA = False
+USE_QLORA = True
+
+## Load model
+
+# Three options for training, from the lowest precision training to the highest precision training:
+# - QLora
+# - Standard Lora
+# - Full fine-tuning
+if USE_QLORA or USE_LORA:
+    if USE_QLORA:
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=True, bnb_4bit_quant_type="nf4", bnb_4bit_compute_dtype=torch.float16
+        )
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        MODEL_ID,
+        torch_dtype=torch.float16,
+        quantization_config=bnb_config,
+    )
+else:
+    # for full fine-tuning, we can speed up the model using Flash Attention
+    # only available on certain devices, see https://github.com/Dao-AILab/flash-attention?tab=readme-ov-file#installation-and-features
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        MODEL_ID,
+        torch_dtype=torch.float16,
+        _attn_implementation="flash_attention_2",
+    )
+
+
+from peft import LoraConfig, prepare_model_for_kbit_training, get_peft_model
+
+def find_all_linear_names(model):
+    cls = torch.nn.Linear
+    lora_module_names = set()
+    multimodal_keywords = ['multi_modal_projector', 'vision_model']
+    for name, module in model.named_modules():
+        if any(mm_keyword in name for mm_keyword in multimodal_keywords):
+            continue
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if 'lm_head' in lora_module_names: # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+
+lora_config = LoraConfig(
+    r=8,
+    lora_alpha=8,
+    lora_dropout=0.1,
+    target_modules=find_all_linear_names(model),
+    init_lora_weights="gaussian",
+)
+
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, lora_config)
+
+from torch.utils.data import Dataset
+from typing import Any, Dict
+import random
+
+class LlavaDataset(Dataset):
+    """
+    PyTorch Dataset for LLaVa. This class takes a HuggingFace Dataset as input.
+
+    Each row, consists of image path(png/jpg/jpeg) and ground truth data (json/jsonl/txt).
+    """
+
+    def __init__(
+        self,
+        dataset_name_or_path: str,
+        split: str = "train",
+        sort_json_key: bool = True,
+    ):
+        super().__init__()
+
+        self.split = split
+        self.sort_json_key = sort_json_key
+
+        self.dataset = load_dataset(dataset_name_or_path, split=self.split)
+        self.dataset = self.dataset.take(100)
+        self.dataset_length = len(self.dataset)
+
+        self.gt_token_sequences = []
+        for sample in self.dataset:
+            ground_truth = json.loads(sample["ground_truth"])
+            if "gt_parses" in ground_truth:  # when multiple ground truths are available, e.g., docvqa
+                assert isinstance(ground_truth["gt_parses"], list)
+                gt_jsons = ground_truth["gt_parses"]
+            else:
+                assert "gt_parse" in ground_truth and isinstance(ground_truth["gt_parse"], dict)
+                gt_jsons = [ground_truth["gt_parse"]]
+
+            self.gt_token_sequences.append(
+                [
+                    self.json2token(
+                        gt_json,
+                        sort_json_key=self.sort_json_key,
+                    )
+                    for gt_json in gt_jsons  # load json from list of json
+                ]
+            )
+
+    def json2token(self, obj: Any, sort_json_key: bool = True):
+        """
+        Convert an ordered JSON object into a token sequence
+        """
+        if type(obj) == dict:
+            if len(obj) == 1 and "text_sequence" in obj:
+                return obj["text_sequence"]
+            else:
+                output = ""
+                if sort_json_key:
+                    keys = sorted(obj.keys(), reverse=True)
+                else:
+                    keys = obj.keys()
+                for k in keys:
+                    output += (
+                        fr"<s_{k}>"
+                        + self.json2token(obj[k], sort_json_key)
+                        + fr"</s_{k}>"
+                    )
+                return output
+        elif type(obj) == list:
+            return r"<sep/>".join(
+                [self.json2token(item, sort_json_key) for item in obj]
+            )
+        else:
+            obj = str(obj)
+            return obj
+
+    def __len__(self) -> int:
+        return self.dataset_length
+
+    def __getitem__(self, idx: int) -> Dict:
+        """
+        Returns one item of the dataset.
+
+        Returns:
+            image : the original Receipt image
+            target_sequence : tokenized ground truth sequence
+        """
+        sample = self.dataset[idx]
+
+        # inputs
+        image = sample["image"]
+        target_sequence = random.choice(self.gt_token_sequences[idx])  # can be more than one, e.g., DocVQA Task 1
+
+        return image, target_sequence
+    
+
+train_dataset = LlavaDataset("naver-clova-ix/cord-v2",  split="train", sort_json_key=False)
+val_dataset = LlavaDataset("naver-clova-ix/cord-v2", split="validation", sort_json_key=False)
+
+counter = 0
+for idx in range(len(train_dataset)):
+    image, target_sequence = train_dataset[idx]
+    # print(f"[INST] <image>\nExtract JSON [\INST] {target_sequence}")
+    counter +=1
+    if counter == 10:
+        break
+
+def train_collate_fn(examples):
+    images = []
+    texts = []
+    for example in examples:
+        image, ground_truth = example
+        images.append(image)
+        # TODO: in the future we can replace this by processor.apply_chat_template
+        # prompt = f"[INST] <image>\nExtract JSON ASSISTANT: [\INST] {ground_truth}"
+        prompt = f"<s> <image>\nExtract JSON ASSISTANT </s> {ground_truth}"
+        texts.append(prompt)
+
+    batch = processor(text=texts, images=images, padding=True, truncation=True, max_length=MAX_LENGTH, return_tensors="pt")
+
+    labels = batch["input_ids"].clone()
+    labels[labels == processor.tokenizer.pad_token_id] = -100
+    batch["labels"] = labels
+
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    pixel_values = batch["pixel_values"]
+    image_sizes = batch["image_sizes"]
+    labels = batch["labels"]
+
+    return input_ids, attention_mask, pixel_values, image_sizes, labels
+
+
+def eval_collate_fn(examples):
+    # we only feed the prompt to the model
+    images = []
+    texts = []
+    answers = []
+    for example in examples:
+        image, ground_truth = example
+        images.append(image)
+        # TODO: in the future we can replace this by processor.apply_chat_template
+        prompt = f"<s> <image>\nExtract JSON ASSISTANT </s>"
+        texts.append(prompt)
+        answers.append(ground_truth)
+
+    batch = processor(text=texts, images=images, return_tensors="pt", padding=True)
+
+    input_ids = batch["input_ids"]
+    attention_mask = batch["attention_mask"]
+    pixel_values = batch["pixel_values"]
+    image_sizes = batch["image_sizes"]
+
+    return input_ids, attention_mask, pixel_values, image_sizes, answers
+
+
+import lightning as L
+from torch.utils.data import DataLoader
+import re
+from nltk import edit_distance
+import numpy as np
+
+
+class LlavaModelPLModule(L.LightningModule):
+    def __init__(self, config, processor, model):
+        super().__init__()
+        self.config = config
+        self.processor = processor
+        self.model = model
+
+        self.batch_size = config.get("batch_size")
+
+    def training_step(self, batch, batch_idx):
+
+        input_ids, attention_mask, pixel_values, image_sizes, labels = batch
+
+        outputs = self.model(input_ids=input_ids,
+                            attention_mask=attention_mask,
+                            pixel_values=pixel_values,
+                            image_sizes=image_sizes,
+                            labels=labels
+                          )
+        loss = outputs.loss
+
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx, dataset_idx=0):
+
+        input_ids, attention_mask, pixel_values, image_sizes, answers = batch
+
+        # autoregressively generate token IDs
+        generated_ids = self.model.generate(input_ids=input_ids, attention_mask=attention_mask,
+                                       pixel_values=pixel_values, image_sizes=image_sizes, max_new_tokens=MAX_LENGTH)
+        # turn them back into text, chopping of the prompt
+        # important: we don't skip special tokens here, because we want to see them in the output
+        predictions = self.processor.batch_decode(generated_ids[:, input_ids.size(1):], skip_special_tokens=True)
+
+        scores = []
+        for pred, answer in zip(predictions, answers):
+            pred = re.sub(r"(?:(?<=>) | (?=</s_))", "", pred)
+            scores.append(edit_distance(pred, answer) / max(len(pred), len(answer)))
+
+            if self.config.get("verbose", False) and len(scores) == 1:
+                print(f"Prediction: {pred}")
+                print(f"    Answer: {answer}")
+                print(f" Normed ED: {scores[0]}")
+
+        self.log("val_edit_distance", np.mean(scores))
+
+        return scores
+
+    def configure_optimizers(self):
+        # you could also add a learning rate scheduler if you want
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.config.get("lr"))
+
+        return optimizer
+
+    def train_dataloader(self):
+        return DataLoader(train_dataset, collate_fn=train_collate_fn, batch_size=self.batch_size, shuffle=True, num_workers=NUM_WORKERS)
+
+    def val_dataloader(self):
+        return DataLoader(val_dataset, collate_fn=eval_collate_fn, batch_size=self.batch_size, shuffle=False, num_workers=NUM_WORKERS)
+    
+
+config = {"max_epochs": 1,
+          # "val_check_interval": 0.2, # how many times we want to validate during an epoch
+          "check_val_every_n_epoch": 1,
+          "gradient_clip_val": 1.0,
+          "accumulate_grad_batches": 8,
+          "lr": 1e-4,
+          "batch_size": 1,
+          # "seed":2022,
+          "num_nodes": 1,
+          "warmup_steps": 50,
+          "result_path": "./result",
+          "verbose": True,
+          "num_workers": NUM_WORKERS
+}
+
+model_module = LlavaModelPLModule(config, processor, model)
+
+from lightning.pytorch.callbacks import Callback
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+
+from huggingface_hub import HfApi
+
+api = HfApi()
+
+class PushToHubCallback(Callback):
+    def on_train_epoch_end(self, trainer, pl_module):
+        print(f"Pushing model to the hub, epoch {trainer.current_epoch}")
+        pl_module.model.push_to_hub(REPO_ID,
+                                    commit_message=f"Training in progress, epoch {trainer.current_epoch}")
+    def on_train_end(self, trainer, pl_module):
+        print(f"Pushing model to the hub after training")
+        pl_module.processor.push_to_hub(REPO_ID,
+                                    commit_message=f"Training done")
+        pl_module.model.push_to_hub(REPO_ID,
+                                    commit_message=f"Training done")
+
+early_stop_callback = EarlyStopping(monitor="val_edit_distance", patience=3, verbose=False, mode="min")
+
+trainer = L.Trainer(
+        accelerator="gpu",
+        strategy="pp",
+        devices=2,
+        max_epochs=config.get("max_epochs"),
+        accumulate_grad_batches=config.get("accumulate_grad_batches"),
+        check_val_every_n_epoch=config.get("check_val_every_n_epoch"),
+        gradient_clip_val=config.get("gradient_clip_val"),
+        precision="16-mixed",
+        limit_val_batches=5,
+        num_sanity_val_steps=0,
+        # logger=wandb_logger,
+        callbacks=[
+            PushToHubCallback(),
+            early_stop_callback],
+)
+
+trainer.fit(model_module)
